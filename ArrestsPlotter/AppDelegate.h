95242f2cf09089f1e80528924b6c9678dc6a8156
Z <z@Zs-iMac.local>
2017-09-28 17:32:44 +0300

    Initial Commit

---
+++ ArrestsPlotter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArrestsPlotter
+//
+//  Created by Z on 28.09.17.
+//  Copyright © 2017 KonstantinBulavenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
